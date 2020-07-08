f5563e22d182babc33d56a128523c60c45a8192c
jsheen <jsheen@sas.upenn.edu>
2020-07-08 13:21:30 -0400

    python power analyses

---
+++ NPI_study/EoN/code/get_log_ratios.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  8 09:16:24 2020
+
+@author: Justin
+
+@description: a script to get the number of infected in treatment clusters, the
+              number of infected in control clusters, the number of recovered 
+              in treatment clusters, the number of recovered in control 
+              clusters, and the log ratios of infected in treatment vs. control
+              and log ratios of recovered in treatment vs. control, according
+              to the final_statuses of the epidemic simulations run.
+
+@input: .csvs of final statuses of epidemic simulations in the csvs folder. The
+        naming convention of each result cell is:
+            (node)_(comm_dex)_(treated)_(enrolled)_(final_status)
+            
+@output: information described in @description above, in a .csv format
+"""
+
+# Import libraries and set input and output folders ---------------------------
+import pandas as pd
+import numpy as np
+input_folder = "/Users/Justin/SW-CRT-outbreak/NPI_study/EoN/code_output/csvs/"
+output_folder = "/Users/Justin/SW-CRT-outbreak/NPI_study/EoN/code_output/log_ratios/"
+
+# Input log ratios wanted -----------------------------------------------------
+effects = [[0.5, 100, 0.04, 0],
+           [0.5, 100, 0.04, 0.95],
+           [0.7, 100, 0.04, 0],
+           [0.7, 100, 0.04, 0.95],
+           [0.9, 100, 0.04, 0],
+           [0.9, 100, 0.04, 0.95]]
+
+# Loop through each effect ----------------------------------------------------
+for effect in effects:
+    cluster_coverage = effect[0]
+    num_comm = effect[1]
+    beta = effect[2]
+    direct_NPIE = effect[3]
+    
+    # Input .csv file of final statuses ---------------------------------------
+    filename = input_folder + str(cluster_coverage) + "_" + str(num_comm) + "_" + str(beta) + "_" + str(direct_NPIE) + "/batch_res.csv"
+    df = pd.read_csv(filename, header=None, names=range(11000), sep=',')
+    
+    # Loop through simulations ------------------------------------------------
+    infected_treatment = []
+    infected_control = []
+    recovered_treatment = []
+    recovered_control = []
+    for row_num in range(500):
+        print("Sim. number: " + str(row_num))
+        sim_infect_treatment = 0
+        sim_infect_control = 0
+        sim_recover_treatment = 0
+        sim_recover_control = 0
+        for col_num in range(2, 11000):
+            if not pd.isnull(df[col_num][row_num]):
+                res = df[col_num][row_num].split("_")
+                # If enrolled, and infected -----------------------------------
+                if res[3] == '1' and res[4] == 'I': 
+                    # If treated ----------------------------------------------
+                    if res[2] == '1':
+                        sim_infect_treatment += 1
+                    else:
+                    # If control ----------------------------------------------
+                        sim_infect_control += 1
+                # If enrolled, and recovered ----------------------------------
+                elif res[3] == '1' and res[4] == 'R':
+                    # If treated ----------------------------------------------
+                    if res[2] == '1':
+                        sim_recover_treatment += 1
+                    # If control ----------------------------------------------
+                    else:
+                        sim_recover_control += 1
+        infected_treatment.append(sim_infect_treatment)
+        infected_control.append(sim_infect_control)
+        recovered_treatment.append(sim_recover_treatment)
+        recovered_control.append(sim_recover_control)
+    log_ratios_infect = [np.log((i_t + 1) / (i_c + 1)) for i_t,i_c in zip(infected_treatment, infected_control)]
+    log_ratios_recover = [np.log((r_t + 1) / (r_c + 1)) for r_t,r_c in zip(recovered_treatment, recovered_control)]
+    
+    # Check for errors --------------------------------------------------------
+    if (len(infected_treatment) != len(infected_control) or
+       len(infected_control) != len(recovered_treatment) or
+       len(recovered_treatment) != len(recovered_control) or
+       len(recovered_control) != len(log_ratios_infect) or
+       len(log_ratios_infect) != len(log_ratios_recover)):
+           raise NameError("Lists are not the same size.")
+    
+    # Write output ------------------------------------------------------------
+    filename = output_folder + str(cluster_coverage) + "_" + str(num_comm) + "_" + str(beta) + "_" + str(direct_NPIE) + ".csv"
+    with open(filename, 'w') as out_f:
+        out_f.write("I_t" + ",")
+        out_f.write("I_c" + ",")
+        out_f.write("R_t" + ",")
+        out_f.write("R_c" + ",")
+        out_f.write("log_ratios_infect" + ",")
+        out_f.write("log_ratios_recover" + ",")
+        out_f.write("\n")
+        for l in range(len(infected_treatment)):
+            out_f.write(str(infected_treatment[l]) + ",")
+            out_f.write(str(infected_control[l]) + ",")
+            out_f.write(str(recovered_treatment[l]) + ",")
+            out_f.write(str(recovered_control[l]) + ",")
+            out_f.write(str(log_ratios_infect[l]) + ",")
+            out_f.write(str(log_ratios_recover[l]) + ",")
+            out_f.write("\n")
---
+++ NPI_study/EoN/code/power_analysis.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  8 11:09:12 2020
+
+@author: Justin
+
+@description: a script to conduct a power analysis on the log ratio information
+              recorded in the log_ratios folder. Each effect is compared to a
+              null distribution to calculate the power. A histogram is 
+              generated with the information of the difference between
+              distributions.
+
+@input:
+    effects: list of lists. Each list has parameter information of the effect
+             to compare to the null distribution. The naming convention is:
+                 (cluster_coverage)_(num_comm)_(beta)_(direct_NPIE)
+
+@output:
+    histogram_comparison: a histogram recording the power, and general 
+                          differences between the null and effect distributions
+"""
+
+# Import libraries and set input and output folders ---------------------------
+import numpy as np
+import pandas as pd
+import math
+import matplotlib.patches as mpatches
+import matplotlib.pyplot as plt
+input_folder = "/Users/Justin/SW-CRT-outbreak/NPI_study/EoN/code_output/log_ratios/"
+output_folder = "/Users/Justin/SW-CRT-outbreak/NPI_study/EoN/code_output/log_ratios_plot/"
+
+# Prepare list of effects to compare to null distribution ---------------------
+effects = [[0.5, 40, 0.04, 0.95],
+          [0.7, 40, 0.04, 0.95],
+          [0.9, 40, 0.04, 0.95],
+          [0.5, 70, 0.04, 0.95],
+          [0.7, 70, 0.04, 0.95],
+          [0.9, 70, 0.04, 0.95],
+          [0.5, 100, 0.04, 0.95],
+          [0.7, 100, 0.04, 0.95],
+          [0.9, 100, 0.04, 0.95],
+          [0.9, 100, 0.04, 0.8]]
+
+for effect in effects:
+    # Load effect parameters --------------------------------------------------
+    cluster_coverage = effect[0]
+    num_comm = effect[1]
+    beta = effect[2]
+    direct_NPIE = effect[3]
+    
+    # Load null distribution --------------------------------------------------
+    null_filename = input_folder + str(cluster_coverage) + "_" + str(num_comm) + "_" + str(beta) + "_" + str(0) + ".csv"
+    null = pd.read_csv(null_filename, sep=',')
+    
+    # Load effect distribution ------------------------------------------------
+    effect_filename = input_folder + str(cluster_coverage) + "_" + str(num_comm) + "_" + str(beta) + "_" + str(direct_NPIE) + ".csv"
+    effect = pd.read_csv(effect_filename, sep=',')
+    
+    # Get log ratio of number of infections information -----------------------
+    log_ratio_null = null.log_ratios_infect
+    log_ratio_null = [float(elem) for elem in log_ratio_null]
+    log_ratio_effect = effect.log_ratios_infect
+    log_ratio_effect = [float(elem) for elem in log_ratio_effect]
+    
+    # Calculate power ---------------------------------------------------------
+    log_ratio_null = np.sort(log_ratio_null)
+    p_val = log_ratio_null[math.ceil(0.05 * 500)]
+    log_ratio_effect = np.sort(log_ratio_effect)
+    num_below = min(np.where(log_ratio_effect > p_val)[0])
+    power = (num_below / 500) * 100
+    
+    # Plot histogram comparison -----------------------------------------------
+    plt.hist(log_ratio_null, bins=10, alpha=0.5, label="null", color="blue")
+    plt.hist(log_ratio_effect, bins=10, alpha=0.5, label="effect", color="orange")
+    plt.axvline(x = p_val, linestyle='--', color="red")
+    plt.ylabel('Frequency')
+    plt.xlabel('ln((num_infect_treatment + 1) / (num_infect_control + 1))')
+    null_patch = mpatches.Patch(color='blue', label='null')
+    effect_patch = mpatches.Patch(color='orange', label='effect')
+    power_patch = mpatches.Patch(color='red', linestyle="--", label='power: ' + str(round(power, 2)))
+    statistics_patch = mpatches.Patch(color='white', label="median null: " + str(round(np.median(log_ratio_null), 2)) + "\nmedian effect: " + str(round(np.median(log_ratio_effect), 2)))
+    plt.legend(loc="upper right", handles=[null_patch, effect_patch, power_patch, statistics_patch], prop={'size': 6})
+    plot_filename = output_folder + str(cluster_coverage) + "_" + str(num_comm) + "_" + str(beta) + "_" + str(direct_NPIE) + ".png"
+    plt.savefig(plot_filename, dpi=1000)
+    plt.clf()
